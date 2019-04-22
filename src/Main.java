16a77e9b44c9638a148e4a545cb5779e13b5c6f9
Dale Holstein <holstein30@live.com>
2019-04-22 19:26:10 -0400

    calculating score for game - refactor next

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if(gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Final Score: " + finalScore);
+        }
+
+        score = 1000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        if(gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Final Score: " + finalScore);
+        }
     }
 }
