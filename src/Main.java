bc5c6fe259a9dda4f4d5dae43aacd0f85aec77f9
Dale Holstein <holstein30@live.com>
2019-04-23 19:24:38 -0400

    slight refactor

--- src/Main.java
+++ src/Main.java
@@ -38,15 +38,19 @@ public class Main {
     }
 
     public static int calculateHighScorePosition(int playerScore) {
+
+        int position = 4;
+
         if (playerScore >= 1000) {
-            return 1;
+            position = 1;
         } else if (playerScore >= 500) {
-            return 2;
+            position = 2;
         } else if (playerScore >= 100) {
-            return 3;
-        } else {
-            return 4;
+            position = 3;
         }
+
+        return position;
+
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
