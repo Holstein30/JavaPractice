cd079021cdf67c1731da06edad88a09b6969d599
Dale Holstein <holstein30@live.com>
2019-04-24 13:39:53 -0400

    moved methods into thier own class

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,24 @@
 public class Main {
 
     public static void main(String[] args) {
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
 
-        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
-        System.out.println("Score: " + highScore);
+        HighScore.newScores();
+        
+        int highScorePosition = HighScore.calculateHighScorePosition(1500);
+        HighScore.displayHighScorePosition("John", highScorePosition);
 
-        score = 1000;
-        levelCompleted = 8;
-        bonus = 200;
+        highScorePosition = HighScore.calculateHighScorePosition(800);
+        HighScore.displayHighScorePosition("Jim", highScorePosition);
 
-        highScore = calculateScore(gameOver, score, levelCompleted, bonus);
-        System.out.println("Score: " + highScore);
+        highScorePosition = HighScore.calculateHighScorePosition(100);
+        HighScore.displayHighScorePosition("Jeff", highScorePosition);
 
-        int highScorePosition = calculateHighScorePosition(1500);
-        displayHighScorePosition("John", highScorePosition);
+        highScorePosition = HighScore.calculateHighScorePosition(50);
+        HighScore.displayHighScorePosition("James", highScorePosition);
 
-        highScorePosition = calculateHighScorePosition(800);
-        displayHighScorePosition("Jim", highScorePosition);
+        highScorePosition = HighScore.calculateHighScorePosition(1000);
+        HighScore.displayHighScorePosition("Jacob", highScorePosition);
 
-        highScorePosition = calculateHighScorePosition(100);
-        displayHighScorePosition("Jeff", highScorePosition);
 
-        highScorePosition = calculateHighScorePosition(50);
-        displayHighScorePosition("James", highScorePosition);
-
-        highScorePosition = calculateHighScorePosition(1000);
-        displayHighScorePosition("Jacob", highScorePosition);
-
-    }
-
-    public static void displayHighScorePosition(String playerName, int highScorePosition) {
-        System.out.println("Name: " + playerName + " Position: " + highScorePosition);
-    }
-
-    public static int calculateHighScorePosition(int playerScore) {
-
-        int position = 4;
-
-        if (playerScore >= 1000) {
-            position = 1;
-        } else if (playerScore >= 500) {
-            position = 2;
-        } else if (playerScore >= 100) {
-            position = 3;
-        }
-
-        return position;
-
-    }
-
-    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
-        if(gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-            finalScore += 1000;
-            return finalScore;
-        }
-
-        return -1;
     }
 }
