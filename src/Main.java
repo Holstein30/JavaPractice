f1c013022c806ba419643dd99df90dd1a4bd2dd2
Dale Holstein <holstein30@live.com>
2019-04-28 11:56:50 -0400

    number of days in months corrected for leap years

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
+        int result = LeapYear.getDaysInMonth(1, 2020);
+        System.out.println(result);
+        result = LeapYear.getDaysInMonth(2, 2020);
+        System.out.println(result);
+        result = LeapYear.getDaysInMonth(2, 2018);
+        System.out.println(result);
+        result = LeapYear.getDaysInMonth(-1,2020);
+        System.out.println(result);
+        result = LeapYear.getDaysInMonth(1, -2020);
+        System.out.println(result);
 
-        SwitchStatement.switchChars("a");
-        SwitchStatement.switchChars("b");
-        SwitchStatement.switchChars("e");
-
-//        boolean answer = PlayingCat.isCatPlaying(true, 10);
-//        System.out.println(answer);
-//        answer = PlayingCat.isCatPlaying(false, 36);
-//        System.out.println(answer);
-//        answer = PlayingCat.isCatPlaying(false,35);
-//        System.out.println(answer);
-
-//        IntEqualityPrinter.printEqual(1, 1, 1);
-//        IntEqualityPrinter.printEqual(1, 1, 2);
-//        IntEqualityPrinter.printEqual(-1,-1,-1);
-//        IntEqualityPrinter.printEqual(1,2,3);
-
-//     MinsToYearsAndDaysCalculator.printYearsAndDays(525600);
-//     MinsToYearsAndDaysCalculator.printYearsAndDays(-1);
-//     MinsToYearsAndDaysCalculator.printYearsAndDays(0);
-//     MinsToYearsAndDaysCalculator.printYearsAndDays(1051200);
-//     MinsToYearsAndDaysCalculator.printYearsAndDays(561600);
-
-//        String answer = GetDuration.getDurationString(143, 59);
-//        System.out.println(answer);
-//
-//        answer = GetDuration.getDurationString(4000);
-//        System.out.println(answer);
-
-//        HighScore.newScores();
-//
-//        int highScorePosition = HighScore.calculateHighScorePosition(1500);
-//        HighScore.displayHighScorePosition("John", highScorePosition);
-//
-//        highScorePosition = HighScore.calculateHighScorePosition(800);
-//        HighScore.displayHighScorePosition("Jim", highScorePosition);
-//
-//        highScorePosition = HighScore.calculateHighScorePosition(100);
-//        HighScore.displayHighScorePosition("Jeff", highScorePosition);
-//
-//        highScorePosition = HighScore.calculateHighScorePosition(50);
-//        HighScore.displayHighScorePosition("James", highScorePosition);
-//
-//        highScorePosition = HighScore.calculateHighScorePosition(1000);
-//        HighScore.displayHighScorePosition("Jacob", highScorePosition);
-//
-//        System.out.println("\n -------------------------- \n");
-//
-//        imperialToMetric.calcFeetAndInchesToCentimeters(6, 0);
-//        imperialToMetric.calcFeetAndInchesToCentimeters(100);
-//        imperialToMetric.calcFeetAndInchesToCentimeters(-1, 1);
-//        imperialToMetric.calcFeetAndInchesToCentimeters(2, 14);
-//        imperialToMetric.calcFeetAndInchesToCentimeters(-12);
-//        imperialToMetric.calcFeetAndInchesToCentimeters(8, -1);
     }
 }
