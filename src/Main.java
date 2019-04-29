20341f38d909841ae5c59027bc9dbbd5ae5c119d
Dale Holstein <holstein30@live.com>
2019-04-29 18:01:39 -0400

    method for determining if a number is perfect

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        FactorPrinter.printFactors(6);
-        FactorPrinter.printFactors(32);
-        FactorPrinter.printFactors(10);
-        FactorPrinter.printFactors(-1);
+        boolean result = PerfectNumber.isPerfectNumber(6);
+        System.out.println(result);
+        result = PerfectNumber.isPerfectNumber(28);
+        System.out.println(result);
+        result = PerfectNumber.isPerfectNumber(5);
+        System.out.println(result);
+        result = PerfectNumber.isPerfectNumber(-1);
+        System.out.println(result);
     }
 }
