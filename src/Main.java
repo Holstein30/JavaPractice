091607ccdcafb87f25989ef0a1908cf3acfc3db1
Dale Holstein <holstein30@live.com>
2019-04-30 18:41:57 -0400

    NumberToWords methodworking

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        NumberToWords.numberToWords(100);
-
-        NumberToWords.getDigitCount(100);
+        NumberToWords.numberToWords(123);
+        NumberToWords.numberToWords(1010);
+        NumberToWords.numberToWords(1000);
+        NumberToWords.numberToWords(-12);
     }
 }
