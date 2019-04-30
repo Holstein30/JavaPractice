15fc7eb30eed4e2b1c3491b1a2f91c27fb9ebe08
Dale Holstein <holstein30@live.com>
2019-04-30 18:02:21 -0400

    method for converting each digit of a positive int into a char

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        boolean result = PerfectNumber.isPerfectNumber(6);
-        System.out.println(result);
-        result = PerfectNumber.isPerfectNumber(28);
-        System.out.println(result);
-        result = PerfectNumber.isPerfectNumber(5);
-        System.out.println(result);
-        result = PerfectNumber.isPerfectNumber(-1);
-        System.out.println(result);
+        NumberToWords.numberToWords(41);
     }
 }
