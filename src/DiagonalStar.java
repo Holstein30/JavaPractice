d4c36fb3252a51e03ea80d1ee3e9a8085a675b0e
Dale Holstein <holstein30@live.com>
2019-05-01 17:39:32 -0400

    diagonal star method solved

--- src/DiagonalStar.java
+++ src/DiagonalStar.java
@@ -5,8 +5,18 @@ public class DiagonalStar {
             return;
         }
 
-        for(int i = 0; i < number; i++){
-            System.out.print("*");
+        for(int i = 1; i <= number; i++){
+            for(int j = 1; j <= number; j++){
+                if((i == 1 || i == (number) || (j == 1 || j == (number)) || (j == (number - i) + 1) || j == i)){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println("");
         }
     }
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
         DiagonalStar.printSquareStar(5);
+        DiagonalStar.printSquareStar(2);
+        DiagonalStar.printSquareStar(8);
+        DiagonalStar.printSquareStar(12);
     }
 }
