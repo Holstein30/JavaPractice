10e8321cea94160b5020c8cb9f1913c8439365b6
Dale Holstein <holstein30@live.com>
2019-05-01 17:22:18 -0400

    method for printing rows and columns of stars

---
+++ src/DiagonalStar.java
@@ -0,0 +1,12 @@
+public class DiagonalStar {
+    public static void printSquareStar(int number) {
+        if (number < 5) {
+            System.out.println("Invalid Value");
+            return;
+        }
+
+        for(int i = 0; i < number; i++){
+            System.out.print("*");
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,9 @@
+import jdk.jshell.Diag;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        int result = LargestPrime.getLargestPrime(21);
-        System.out.println(result);
-        result = LargestPrime.getLargestPrime(217);
-        System.out.println(result);
-        result = LargestPrime.getLargestPrime(0);
-        System.out.println(result);
-        result = LargestPrime.getLargestPrime(45);
-        System.out.println(result);
-        result = LargestPrime.getLargestPrime(-1);
-        System.out.println(result);
+        DiagonalStar.printSquareStar(5);
     }
 }
