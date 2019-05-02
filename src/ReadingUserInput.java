c9facdba2bd0d744579cb8a0c63319a179dbc946
Dale Holstein <holstein30@live.com>
2019-05-02 13:12:13 -0400

    simple name and age calculation using scanners to grab user input

--- src/ReadingUserInput.java
+++ src/ReadingUserInput.java
@@ -3,10 +3,18 @@ import java.util.Scanner;
 public class ReadingUserInput {
     public static void scannerDemo(){
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Year of birth: ");
+        int yearOfBirth = scanner.nextInt();
+        int age = ageCalc(yearOfBirth);
         System.out.println("Name: ");
         String name = scanner.nextLine();
         System.out.println("Your name is: " + name);
+        System.out.println("You are " + age + " years old");
 
         scanner.close();
+    }
+
+    public static int ageCalc(int yearOfBirth){
+        return 2019 - yearOfBirth;
     }
 }
