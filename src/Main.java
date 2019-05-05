085241e3135f1130d8fa7df7c4b9aed2eeea9f10
Dale Holstein <holstein30@live.com>
2019-05-05 12:03:23 -0400

    simple OO calculator

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,18 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
-        Car porsche = new Car();
-        Car lamborghini = new Car();
 
-        porsche.setModel("Cayman");
-        lamborghini.setModel("Diablo");
 
-        System.out.println("Porsche " + porsche.getModel());
+        SimpleCalculator calculator = new SimpleCalculator();
+
+        calculator.setFirstNumber(5);
+        calculator.setSecondNumber(12);
+
+        double first = calculator.getFirstNumber();
+        double second = calculator.getSecondNumber();
+        System.out.println(first);
+        System.out.println(second);
+
+
     }
 }
