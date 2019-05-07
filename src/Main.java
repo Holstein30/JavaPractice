ec4212027da22febeec2d3c60eeada0bb4f9f2b0
Dale Holstein <holstein30@live.com>
2019-05-07 14:49:13 -0400

    Point class with constructors, getters, and setters created

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,14 @@ public class Main {
         Wall wall = new Wall(5, 4);
 
         System.out.println("Area = " + wall.getArea());
+        System.out.println("Width = " + wall.getWidth());
+        System.out.println("Height = " + wall.getHeight());
+
+        wall.setHeight(-1.5);
+
+        System.out.println("Width = " + wall.getWidth());
+        System.out.println("Height = " + wall.getHeight());
+        System.out.println("Area = " + wall.getArea());
 
     }
 }
