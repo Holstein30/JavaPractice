1c1b857c73b604c6c1b8094483aaed278e22f2d0
Dale Holstein <holstein30@live.com>
2019-05-09 13:00:15 -0400

    add and subtract methods for complex numbers created

--- src/ComplexNumber.java
+++ src/ComplexNumber.java
@@ -20,4 +20,19 @@ public class ComplexNumber {
         this.imaginary += imaginary;
     }
 
+    public void add(ComplexNumber complexNumber){
+        this.real += complexNumber.getReal();
+        this.imaginary += complexNumber.getImaginary();
+    }
+
+    public void subtract(double real, double imaginary){
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+    public void subtract(ComplexNumber complexNumber){
+        this.real -= complexNumber.getReal();
+        this.imaginary -= complexNumber.getImaginary();
+    }
+
 }
