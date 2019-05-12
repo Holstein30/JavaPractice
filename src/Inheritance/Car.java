363329de8b5de05f32ba0371c872715e8ae5ad84
Dale Holstein <holstein30@live.com>
2019-05-12 13:32:53 -0400

    Car class created - inherits from Vehicle class

---
+++ src/Inheritance/Car.java
@@ -0,0 +1,31 @@
+package Inheritance;
+
+public class Car extends Vehicle {
+    private int wheels;
+    private int doors;
+    private int gears;
+    private boolean manual;
+    private int currentGear;
+
+    public Car(String make, String model, String bodyStyle, int wheels, int doors, int gears, boolean manual) {
+        super(make, model, bodyStyle);
+        this.wheels = wheels;
+        this.doors = doors;
+        this.gears = gears;
+        this.manual = manual;
+        this.currentGear = 1;
+    }
+
+    public void changeGear(int currentGear) {
+        this.currentGear = currentGear;
+        System.out.println("Current Gear = " + this.currentGear);
+    }
+
+    public void changeSpeed(int speed, int direction){
+        move(speed, direction);
+        System.out.println("Car.changeSpeed() : ");
+    }
+
+
+
+}
