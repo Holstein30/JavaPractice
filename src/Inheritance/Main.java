9f0ee8f42855d510a51b30720e5af0fe822a04f4
Dale Holstein <holstein30@live.com>
2019-05-13 14:27:02 -0400

    Cuboid class createdthat extends Rectangle

--- src/Inheritance/Main.java
+++ src/Inheritance/Main.java
@@ -4,17 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Circle circle = new Circle(3.75);
-
-        System.out.println("Radius = " + circle.getRadius());
-        System.out.println("Area = " + circle.getArea());
-
-        Cylinder cylinder = new Cylinder(5.55, 7.25);
-
-        System.out.println("Radius = " + cylinder.getRadius());
-        System.out.println("Height = " + cylinder.getHeight());
-        System.out.println("Area = " + cylinder.getArea());
-        System.out.println("Volume = " + cylinder.getVolume());
+        
 
         }
     }
