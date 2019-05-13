fd16e70c82c29f166e11e22c10e38cbf3337e271
Dale Holstein <holstein30@live.com>
2019-05-13 14:27:40 -0400

    All methods validated - all tests passed

--- src/Inheritance/Main.java
+++ src/Inheritance/Main.java
@@ -4,7 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        
+        Rectangle rectangle = new Rectangle(5, 10);
+
+        System.out.println("Width = " + rectangle.getWidth());
+        System.out.println("Length = " + rectangle.getLength());
+        System.out.println("Area = " + rectangle.getArea());
+
+        Cuboid cuboid = new Cuboid(5,10,5);
+
+        System.out.println("Width = " + cuboid.getWidth());
+        System.out.println("Length = " + cuboid.getLength());
+        System.out.println("Area = " + cuboid.getArea());
+        System.out.println("Height = " + cuboid.getHeight());
+        System.out.println("Volume = " + cuboid.getVolume());
 
         }
     }
