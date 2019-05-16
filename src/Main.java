352f774d4e1e7aefaff1aece15c5f8605ee8541e
Dale Holstein <holstein30@live.com>
2019-05-16 18:20:27 -0400

    EnhancedPlayer class created

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-    Player player = new Player();
+//    Player player = new Player();
+//
+//    player.name = "Nako";
+//    player.health = 100;
+//    player.weapon = "Bow";
+//
+//    int damage = 50;
+//
+//    player.loseHealth(damage);
+//    System.out.println("Remaining Health = " + player.healthRemaining());
+//
+//    damage = 50;
+//    player.loseHealth(damage);
+//    System.out.println("Remaining Health = " + player.healthRemaining());
 
-    player.name = "Nako";
-    player.health = 100;
-    player.weapon = "Bow";
+        EnhancedPlayer player = new EnhancedPlayer("Nako", 50, "Bow");
+        System.out.println(player.getHealth());
 
-    int damage = 50;
 
-    player.loseHealth(damage);
-        System.out.println("Remaining Health = " + player.healthRemaining());
-        
 
     }
 }
