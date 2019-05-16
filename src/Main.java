edad46bea784b653d02ae5e802e6184b9f4f638d
Dale Holstein <holstein30@live.com>
2019-05-16 18:23:25 -0400

    encapsulation practice with enhanced player tested and functioning

--- src/Main.java
+++ src/Main.java
@@ -6,23 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-//    Player player = new Player();
-//
-//    player.name = "Nako";
-//    player.health = 100;
-//    player.weapon = "Bow";
-//
-//    int damage = 50;
-//
-//    player.loseHealth(damage);
-//    System.out.println("Remaining Health = " + player.healthRemaining());
-//
-//    damage = 50;
-//    player.loseHealth(damage);
-//    System.out.println("Remaining Health = " + player.healthRemaining());
-
         EnhancedPlayer player = new EnhancedPlayer("Nako", 50, "Bow");
         System.out.println(player.getHealth());
+
+        int damage = 50;
+
+        player.loseHealth(damage);
 
 
 
