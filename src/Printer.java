c4867aea26a6047811dcfc5658aa4166ef08614b
Dale Holstein <holstein30@live.com>
2019-05-17 22:39:53 -0400

    validation added for setting toner level - methods for increasing pages printed and decreasing toner enhanced

--- src/Printer.java
+++ src/Printer.java
@@ -1,19 +1,23 @@
 public class Printer {
-    private int tonerLevel;
+    private int tonerLevel = 10;
     private int pagesPrinted;
     private boolean duplex;
 
     public Printer(int tonerLevel, int pagesPrinted, boolean duplex) {
-        this.tonerLevel = tonerLevel;
+        if(tonerLevel > 0 || tonerLevel < 10){
+            this.tonerLevel = tonerLevel;
+        }
         this.pagesPrinted = pagesPrinted;
         this.duplex = duplex;
     }
 
     public int getTonerLevel() {
+        System.out.println("Toner Level = " + tonerLevel);
         return tonerLevel;
     }
 
     public int getPagesPrinted() {
+        System.out.println("Pages printed = " + pagesPrinted);
         return pagesPrinted;
     }
 
@@ -22,11 +26,13 @@ public class Printer {
     }
 
     public void fillToner(){
+        System.out.println("Toner Filled");
         this.tonerLevel = 10;
     }
 
     public void printPage(){
         if(this.tonerLevel > 0){
+            System.out.println("Page Printed - Toner Decreased");
             this.pagesPrinted += 1;
             this.tonerLevel -= 1;
         } else {
